eb2bb88702ed8fc7897089ff2e2d8c2ba0209551
andrejli <andrej.licko.sk@gmail.com>
2022-06-21 15:12:57 +0200

    comments and TR test

--- tests.py
+++ tests.py
@@ -1,6 +1,11 @@
 import os
 from IO.io_plan import read_plan
+from IO.template_reader import TemplateReader
 
 def test_read_plan():
 	file = os.getcwd()+"/tests/test_plan"
 	assert read_plan(file) == ['Alpha\n', 'Beta\n', 'Gama\n', 'Delta\n']
+
+def test_TR_instance():
+	obj = TemplateReader(filename="template")
+	assert isinstance(obj, TemplateReader) == True
